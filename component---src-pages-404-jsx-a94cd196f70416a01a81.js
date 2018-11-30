30b87db3c3b5d7fffe435da7538438e5d2b3a96e
muhammad Rusdi <muhamamdrusdi@icloud.com>
2018-11-30 22:42:45 +0800

    global style

---
+++ component---src-pages-404-jsx-a94cd196f70416a01a81.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{190:function(e,t,n){"use strict";n.r(t);var a=n(1),u=n.n(a);t.default=function(){return u.a.createElement(u.a.Fragment,null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-a94cd196f70416a01a81.js.map

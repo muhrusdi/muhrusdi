2778a77b70cb1409bd283676b19f21fec61f2ae9
muhammad Rusdi <muhamamdrusdi@icloud.com>
2018-12-30 22:34:26 +0800

    fixed css

--- src/containers/Layout/index.jsx
+++ src/containers/Layout/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import { Styles } from 'reus'
 import './layout.css'
 
 import favicon32 from '../../images/favicon-32x32.png'

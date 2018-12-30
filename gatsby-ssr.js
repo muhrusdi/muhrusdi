94d60cbb9b0a8f2191c8369f14bddec7ce1edf8b
muhammad Rusdi <muhamamdrusdi@icloud.com>
2018-12-31 00:17:18 +0800

    fixed css

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,7 +7,7 @@ import { store } from './src/store'
 import { cache } from 'emotion'
 import { CacheProvider } from '@emotion/core'
 
-export const replaceRenderer = ({bodyComponent, replaceBodyHTMLString }) => {
+export const replaceRenderer = ({ setHeadComponents, bodyComponent, replaceBodyHTMLString }) => {
   const ConnectedBody = () => (
     <Provider store={store}>
       <CacheProvider value={cache}>
@@ -15,7 +15,19 @@ export const replaceRenderer = ({bodyComponent, replaceBodyHTMLString }) => {
       </CacheProvider>
     </Provider>
   )
-  replaceBodyHTMLString(<ConnectedBody/>)
+
+  // const { html, ids, css } = extractCritical(renderToString(<ConnectedBody/>))
+
+  // const criticalStyle = <style dangerouslySetInnerHTML={{ __html: css }} />
+  // const criticalIds = (
+  //   <script
+  //     dangerouslySetInnerHTML={{
+  //       __html: `window.__EMOTION_CRITICAL_CSS_IDS__ = ${JSON.stringify(ids)};`,
+  //     }}
+  //   />
+  //   )
+  // setHeadComponents([criticalIds, criticalStyle])
+  replaceBodyHTMLString(renderToString(<ConnectedBody/>))
 }
 
 export const wrapPageElement = ({ element, props }) => {

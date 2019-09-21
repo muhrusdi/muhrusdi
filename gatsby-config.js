b17c798ded4fc6a0eeb19228f5f4a47f8d85d6b6
muhrusdi <muh.rusdi@docotel.com>
2019-09-21 14:54:06 +0800

    fix seoa

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        id: process.env.GA_ID,
+        id: process.env.GTAG_ID,
   
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.

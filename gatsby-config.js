dae452f6ab0a2754021100d79ea00de4e1bafc0a
muhrusdi <muh.rusdi@docotel.com>
2019-09-22 14:15:02 +0800

    remove sw

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,12 +20,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     {
-      resolve: "gatsby-plugin-netlify-cache",
-      options: {
-        cachePublic: true
-      }
-    },
-    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
@@ -128,6 +122,7 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
   ],
 }
